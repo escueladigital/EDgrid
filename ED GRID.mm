fc7c9df919d26040f82ee878c0d8c962d31a75c5
escueladigital <ceo@escueladigital.pe.>
2015-03-25 15:54:23 -0500

    ED-GRID RC con Sass
    Versión candidata de ED GRID con Sass. Lista para testear en producción.

---
+++ ED GRID.mm
@@ -0,0 +1,109 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ED GRID" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1426119344800"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="Estructura sass" POSITION="right" ID="ID_1223194559" CREATED="1426119591716" MODIFIED="1426119594609">
+<edge COLOR="#0000ff"/>
+<node TEXT="carpetas y archivos" ID="ID_676172143" CREATED="1426119345390" MODIFIED="1426119601539">
+<node TEXT="tema" ID="ID_944762926" CREATED="1426119350484" MODIFIED="1426120145711">
+<node TEXT="tema" ID="ID_671251560" CREATED="1426120474612" MODIFIED="1426120476807"/>
+<node TEXT="tipografia" ID="ID_870347097" CREATED="1426119764941" MODIFIED="1426119768601"/>
+<node TEXT="botones" ID="ID_533434184" CREATED="1426119406692" MODIFIED="1426119408426"/>
+<node TEXT="imagenes" ID="ID_78670316" CREATED="1426119412667" MODIFIED="1426119414427"/>
+<node TEXT="menu" ID="ID_540688211" CREATED="1426120177737" MODIFIED="1426120179088"/>
+<node TEXT="iconos" ID="ID_378155996" CREATED="1426120179601" MODIFIED="1426120183486"/>
+<node TEXT="variables.scss" ID="ID_909723522" CREATED="1426119608732" MODIFIED="1426119611913"/>
+</node>
+<node TEXT="modulos" ID="ID_988261058" CREATED="1426119363882" MODIFIED="1426120172707"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Caracter&#237;sticas creadas para el cliente
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="base" ID="ID_158357299" CREATED="1426119380340" MODIFIED="1426120137072"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Estilos de inicio, como ed-grid, normalize y otros necesarios
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="ed grid" ID="ID_1477794809" CREATED="1426119550620" MODIFIED="1426119555545"/>
+<node TEXT="normalize" ID="ID_1741170416" CREATED="1426119556578" MODIFIED="1426119560273"/>
+<node TEXT="mixins" ID="ID_1780063432" CREATED="1426119560674" MODIFIED="1426119564752"/>
+</node>
+<node TEXT="layout" ID="ID_1848786520" CREATED="1426119387763" MODIFIED="1426119389778">
+<node TEXT="header" ID="ID_1553891225" CREATED="1426119736172" MODIFIED="1426119738216"/>
+<node TEXT="footer" ID="ID_1040871646" CREATED="1426119738715" MODIFIED="1426119741336"/>
+<node TEXT="main" ID="ID_581178009" CREATED="1426119741817" MODIFIED="1426119743641"/>
+<node TEXT="aside" ID="ID_1297588737" CREATED="1426119743890" MODIFIED="1426119744808"/>
+</node>
+</node>
+<node TEXT="estilos.scss" ID="ID_771115025" CREATED="1426119603034" MODIFIED="1426119608201"/>
+</node>
+</node>
+</map>
