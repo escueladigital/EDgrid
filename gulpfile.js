95dac7e2eb6cb028f52bf4096593562bd6283881
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2015-10-11 13:41:04 -0500

    Se optimizó gulpfile.js para que el uso de Jade sea opcional.
    (Opcional)
    Se puede definir el puerto del livereload desde la terminal para cualquiera de las tareas watch, por ejemplo:
    PORT=8080 gulp watch:all

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * Dependencias
+ */
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
@@ -7,19 +10,38 @@ var sass = require('gulp-sass');
 var jade = require('gulp-jade');
 var connect = require('gulp-connect');
 
-gulp.task('css', function () {
+/**
+ * Variable de entorno.
+ * En la terminal se puede definir de manera opcional el puerto para cualquiera
+ * de las tareas watch, un ejemplo de uso sería:
+ * PORT=8080 gulp watch:all
+ */
+ var PORT = process.env.PORT || 7070;
+
+
+/**
+ * Compila los archivos sass hijos directos de la carpeta `scss/`.
+ * Agrega los prefijos propietarios de los navegadores.
+ * Los archivos CSS generados se guardan en la carpeta `css/`.
+ */
+gulp.task('sass', function () {
   var processors = [
     autoprefixer({ browsers: ['last 2 versions'] })
   ];
 
-  return gulp.src('./scss/styles.scss')
+  return gulp.src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(processors))
     .pipe(gulp.dest('./css'))
     .pipe(connect.reload());
 });
 
-gulp.task('html', function() {
+
+/**
+ * Compila los archivos jade hijos directos de la carpeta `jade/`.
+ * Los archivos HTML generados se guardan en la carpeta raíz del proyecto.
+ */
+gulp.task('jade', function() {
   gulp.src('./jade/*.jade')
     .pipe(jade({
       pretty: true
@@ -28,19 +50,72 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
+
+/**
+ * Recarga el HTML en el navegador.
+ * Creado para quienes no usen Jade.
+ */
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+
+/**
+ * Crea un servidor local livereload
+ * http://localhost:7070
+ */
 gulp.task('connect', function() {
   connect.server({
     // root: '.',
-    port: 7070,
+    port: PORT,
     livereload: true
   });
 });
 
-gulp.task('watch', ['css', 'html'], function () {
-  gulp.watch('./scss/**/*.scss', ['css']);
-  gulp.watch('./jade/**/*.jade', ['html']);
+
+/**
+ * Ejecuta las tareas connect y sass, queda escuchando los cambios de todos
+ * los archivos Sass de la carpeta `scss/` y subcarpetas.
+ */
+gulp.task('watch:sass', ['connect', 'sass'], function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });
 
-gulp.task('start', ['connect', 'watch']);
 
-gulp.task('default', ['css', 'html']);
+/**
+ * Ejecuta las tareas connect y jade, queda escuchando los cambios de todos
+ * los archivos jade de la carpeta `jade/` y subcarpetas.
+ */
+gulp.task('watch:jade', ['connect', 'jade'], function () {
+  gulp.watch('./jade/**/*.jade', ['jade']);
+});
+
+
+/**
+ * Ejecuta las tareas connect y html, queda escuchando los cambios de todos
+ * los archivos HTML de la carpeta raíz del proyecto.
+ * Creado para quienes no usen Jade.
+ */
+gulp.task('watch:html', ['connect', 'html'], function () {
+  gulp.watch('./*.html', ['html']);
+});
+
+
+/**
+ * Ejecuta las tareas watch:html y watch:sass
+ * Creado para quienes no usen Jade.
+ */
+gulp.task('watch:html-sass', ['watch:html', 'watch:sass']);
+
+
+/**
+ * Ejecuta las tareas watch:sass y watch:jade.
+ */
+gulp.task('watch:all', ['watch:sass', 'watch:jade']);
+
+
+/**
+ * Ejecuta la tarea watch:sass.
+ */
+gulp.task('default', ['watch:sass']);
