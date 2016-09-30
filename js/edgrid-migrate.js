5b20d4c61b0b9b55d39953c03513d34fa8fe56ac
Alvaro Felipe <alvaro@escuela.digital>
2016-09-30 16:36:19 -0500

    Refactorizacion y convertidos los nodeList a arrays

--- js/edgrid-migrate.js
+++ js/edgrid-migrate.js
@@ -1,62 +1,81 @@
 (function(d){
-  var selectors = {
+  // parejas de selectores
+  let selectors = {
     grupo: ['grupo','ed-container'],
     caja: ['caja','ed-item'],
     total: ['total', 'full'],
   };
 
-  var addNewSelector = function(selector, newSelector) {
-    var elements = d.querySelectorAll(`.${selector}`);
-    var elementsLength = elements.length;
+  // función para añadir nuevo selector
+  let addNewSelector = function(selector, newSelector) {
+    let elements = d.querySelectorAll(`.${selector}`);
+    let elementsLength = elements.length;
     while (elementsLength--) {
       elements[elementsLength].classList.add(newSelector)
     }
   };
 
-  for (var selector in selectors) {
+  // Recorrer el objeto selector para añadir las clases nuevas
+  for (let selector in selectors) {
     addNewSelector(selectors[selector][0],selectors[selector][1])
   }
 })(document);
 
+
 (function (d) {
-  var oldSizes = {
-    'base-'  : 's-',
-    'movil-' : 's-',
-    'tablet-': 'm-',
-    'web-'   : 'l-',
-    'hd'     : 'xl-'
+  let oldSizes = {
+    'base-'        : 's-',
+    'movil-'       : 's-',
+    'tablet-'      : 'm-',
+    'web-'         : 'l-',
+    'hd'           : 'xl-',
+    'hasta-tablet' : 'to-m',
+    'hasta-web'    : 'to-l',
+    'hasta-hd'     : 'to-xl',
+    'desde-tablet' : 'from-m',
+    'desde-web'    : 'from-l',
+    'desde-hd'     : 'from-hd'
   };
 
+  // función para añadir las nuevas clases
+  let addNewClasses = function(oldSize,newSize){
+    // nodeList de elementos con las clases antiguas
+    let items = d.querySelectorAll(`[class*="${oldSize}"]`);
+    let itemsArray = Array.prototype.slice.apply(items);
+    // array en el que se guardaran las clases antiguas
+    let oldClasses = [];
+    // array en el que se guardarán las nuevas clases
+    let newClasses;
+    let itemsL = items.length;
 
+    // Ciclo para extraer las clases antiguas
+    for (let i = 0; i < itemsL; i++) {
+      // obtener las clases de cada elemento
+      let thisClasses = items[i].classList;
+      let classesArray = Array.prototype.slice.apply(thisClasses);
 
-  var addNewClasses = function(oldSize,newSize){
-    var items = d.querySelectorAll(`[class*="${oldSize}"]`);
-    var oldClasses = [];
-    var newClasses;
-    var itemsL = items.length;
-
-    for (var i = 0; i < itemsL; i++) {
-      var thisClasses = items[i].classList;
-      var classesArray = Array.prototype.slice.apply(thisClasses);
-
-      var extractedClasses = classesArray.filter(function(classItem){
+      // recuperar la clase antigua
+      let extractedClasses = classesArray.filter(function(classItem){
         return classItem.indexOf(oldSize) == 0;
       });
 
+      // Añadir la clase antigua al array oldClasses
       oldClasses.push(extractedClasses[0]);
     }
 
+    // Crear un nuevo array con las nuevas clases
     newClasses = oldClasses.map(function(classItem){
       return classItem.replace(oldSize,newSize)
     });
 
-    items.forEach(function(size, index){
+    // Añadir las nuevas clases a los items
+    itemsArray.forEach(function(size, index){
       size.classList.add(newClasses[index]);
     });
   };
 
-  for (var oldSize in oldSizes) {
+  // añadir las nuevas clases a los items
+  for (let oldSize in oldSizes) {
     addNewClasses(oldSize,oldSizes[oldSize])
   }
-
 })(document);
