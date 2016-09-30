bb06ae81786ffffca219d5ea980d4a4a917cf2e6
Alvaro Felipe <alvaro@escuela.digital>
2016-09-30 16:37:43 -0500

    Añadida deteccion de submenus y mostrar/ocultar submenus

--- js/edgrid-menu.js
+++ js/edgrid-menu.js
@@ -1,19 +1,55 @@
-var edMenu = function(menuId, direction) {
-  var menu = document.getElementById(menuId),
-    toggleButton = document.getElementById(`${menuId}-toggle`);
-  function showMenu() {
-    menu.classList.toggle('show-menu');
-    console.log('click')
+let edMenu = function(navId,menuId) {
+           let nav = document.getElementById(navId),
+              menu = document.getElementById(menuId),
+      toggleButton = document.getElementById(`${navId}-toggle`);
+           console.log(menu);
+
+  function showNav() {
+    nav.classList.toggle('show-menu');
   }
-  if (menu) {
-    menu.classList.add(direction);
+
+  // si el nav y toggle existen mostrar u ocultar menu
+  if (nav) {
     if (toggleButton) {
-      toggleButton.addEventListener('click', showMenu);
+      toggleButton.addEventListener('click', showNav);
     } else {
-      console.error(`Not found id ${menuId}-toggle`)
+      console.error(`Not found ${navId}-toggle Id`)
     }
   } else {
-    console.error(`Not found id ${menuId}`)
+    console.error(`Not found ${navId} Id`)
+  }
+
+  if (menu) {
+    // Detectar submenus
+    let menuItems = menu.querySelectorAll('li');
+    let menuItemsLength = menuItems.length;
+
+    function showSubMenu(e) {
+      e.preventDefault();
+      if (e.target.classList.contains('expand-submenu')) {
+        e.target.classList.toggle('active');
+        e.target.previousElementSibling.classList.toggle('show-submenu');
+      }
+    }
+
+    menu.addEventListener('click', (e) => {
+      showSubMenu(e);
+    });
+
+    while (menuItemsLength--) {
+      let menuItem = menuItems[menuItemsLength];
+      // Detectar si un item es padre de un submenu
+      if (menuItem.querySelector('ul') != null) {
+        menuItem.classList.add('parent-submenu');
+
+        //Crear toggle button para submenus
+        let expandSubmenu = document.createElement('div');
+        expandSubmenu.classList.add('expand-submenu');
+        menuItem.appendChild(expandSubmenu);
+      }
+    }
+  } else {
+    console.error(`Not found ${menuId} Id`)
   }
 };
 
