ba5f044588ec4fbfbcc43de6962b09e37d8a0b95
Alvaro Felipe <alvaro@ed.team>
2018-11-03 19:59:06 -0500

    Agrega nuevas tareas de gulp para la nueva estructura del repositorio

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,6 @@ import sass from 'gulp-sass'
 import plumber from 'gulp-plumber'
 import postcss from 'gulp-postcss'
 import autoprefixer from 'autoprefixer'
-import cssnano from 'cssnano'
 import browserSync from 'browser-sync'
 import sourcemaps from 'gulp-sourcemaps'
 import browserify from 'browserify'
@@ -11,83 +10,212 @@ import babelify from 'babelify'
 import buffer from 'vinyl-buffer'
 import source from 'vinyl-source-stream'
 import rename from 'gulp-rename'
+import pug from 'gulp-pug'
+import watch from 'gulp-watch'
+import minify from 'gulp-minify'
+import imagemin from 'gulp-imagemin'
+import sitemap from 'gulp-sitemap'
+import cachebust from 'gulp-cache-bust'
+import tildeImporter from 'node-sass-tilde-importer'
+import data from 'gulp-data'
+import fs from 'fs'
 
-const server = browserSync.create();
 
-let postcssPlugins = [
-  autoprefixer({browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'}),
-  cssnano({core:true})
-];
+const server = browserSync.create()
 
-let sassOptions = {
-  outputStyle: 'expanded'
-};
+const postcssPlugins = [
+  autoprefixer({browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'})
+]
 
-gulp.task('styles', () =>
-  gulp.src('./test/styles.scss')
-      .pipe(plumber({
-        errorHandler: function (err) {
-          console.log(err);
-          this.emit('end');
-        }
-      }))
-      .pipe(sass(sassOptions))
-      .pipe(postcss(postcssPlugins))
-      .pipe(plumber.stop())
-      .pipe(gulp.dest('./test'))
-      .pipe(server.stream())
-);
+// Compila Sass para desarrollo
+gulp.task('styles-dev', () => {
+  gulp.src('./src/scss/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass({
+      importer: tildeImporter,
+      sourceComments: true,
+      outputStyle: 'expanded'
+    }))
+    .pipe(postcss(postcssPlugins))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(server.stream({match: '**/*.css'}))
+})
 
-gulp.task('compileCoreExpanded', () =>
-  gulp.src('./ed-grid.scss')
-    .pipe(sass(sassOptions))
-    .pipe(postcss([
-      autoprefixer({browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'}),
-      cssnano({core:false})
-    ]))
-    .pipe(gulp.dest('./css'))
-);
+// Compila Sass para producción
+gulp.task('styles-build', () => {
+  gulp.src('./src/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      importer: tildeImporter,
+      outputStyle: 'compressed'
+    }))
+    .pipe(postcss(postcssPlugins))
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(server.stream({match: '**/*.css'}))
+})
 
-gulp.task('compileCoreMinified', () =>
-  gulp.src('./ed-grid.scss')
-    .pipe(sass(sassOptions))
-    .pipe(postcss([
-      autoprefixer({browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'}),
-      cssnano({core:true})
-    ]))
+// Compila versión CSS minificada
+gulp.task('css-build-min', () => {
+  gulp.src('./src/scss/ed-grid.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(postcss(postcssPlugins))
     .pipe(rename('ed-grid.min.css'))
-    .pipe(gulp.dest('./css'))
-);
+    .pipe(gulp.dest('./src/css/'))
+})
 
-gulp.task('scripts', () =>
-  browserify('./js/babel/index.js', {
-      debug: true,
-      standalone: 'edgrid'
+// Compila versión CSS sin minificar
+gulp.task('css-build', () => {
+  gulp.src('./src/scss/ed-grid.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(postcss(postcssPlugins))
+    .pipe(gulp.dest('./src/css/'))
+})
+
+// Compila páginas internas
+gulp.task('pug-dev', () =>
+  gulp.src('./src/pug/pages/**/*.pug')
+    .pipe(plumber())
+    .pipe(data(function () {
+      return JSON.parse(fs.readFileSync('./src/data/casos-de-exito.json'))
+    }))
+    .pipe(pug({
+      pretty: true,
+      basedir: './dev/pug'
+    }))
+    .pipe(gulp.dest('./public'))
+)
+
+// Compila páginas internas minificadas
+gulp.task('pug-build', () =>
+  gulp.src('./src/pug/pages/**/*.pug')
+    .pipe(plumber())
+    .pipe(data(function () {
+      return JSON.parse(fs.readFileSync('./src/data/casos-de-exito.json'))
+    }))
+    .pipe(pug({
+      basedir: './dev/pug'
+    }))
+    .pipe(gulp.dest('./public'))
+)
+
+// Compila JavaScript sin minificar
+gulp.task('scripts-dev', () =>
+  browserify('./src/js/index.js')
+    .transform(babelify, {
+      global: true // permite importar desde afuera (como node_modules)
     })
-    .transform(babelify)
     .bundle()
-    .pipe(source('ed-grid.js'))
+    .on('error', function (err) {
+      console.error(err)
+      this.emit('end')
+    })
+    .pipe(source('scripts.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(minify({
+      ext: {
+        src: '-min.js',
+        min: '.js'
+      }
+    }))
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./js'))
-);
+    .pipe(gulp.dest('./public/js'))
+)
 
-gulp.task('sw', () =>
-  gulp.watch('./**/**.scss', ['styles'])
-);
+// Compila JavaScript minificado
+gulp.task('scripts-build', () =>
+  browserify('./src/js/index.js')
+    .transform(babelify, {
+      global: true // permite importar desde afuera (como node_modules)
+    })
+    .bundle()
+    .on('error', function (err) {
+      console.error(err)
+      this.emit('end')
+    })
+    .pipe(source('scripts.js'))
+    .pipe(buffer())
+    .pipe(minify({
+      ext: {
+        src: '.js',
+        min: '-min.js'
+      }
+    }))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/js'))
+)
 
-gulp.task('default', () => {
+// Copia y comprime imágenes
+gulp.task('images-build', () => {
+  gulp.src('./src/img/**/**')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('./public/assets/img'))
+})
+
+// Copia imágenes para desarrollo
+gulp.task('images-dev', () => {
+  gulp.src('./src/img/**/**')
+    .pipe(gulp.dest('./public/assets/img'))
+})
+
+gulp.task('sitemap', () => {
+  gulp.src('./public/**/*.html', {
+    read: false
+  })
+    .pipe(sitemap({
+      siteUrl: 'https://ed-grid.com' // remplazar por tu dominio
+    }))
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('cache', () => {
+  gulp.src('./public/**/*.html')
+    .pipe(cachebust({
+      type: 'timestamp'
+    }))
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('dev', ['styles-dev', 'pug-dev', 'scripts-dev', 'images-dev'], () => {
   server.init({
     server: {
-      baseDir: './test' //para pruebas con la carpeta test
-    },
+      baseDir: './public'
+    }
+  })
 
-    serveStatic: ['./js']
-  });
+  watch('./src/scss/**/**', () => gulp.start('styles-dev'))
+  watch('./src/js/**/**', () => gulp.start('scripts-dev', server.reload))
+  watch('./src/pug/**/**', () => gulp.start('pug-dev', server.reload))
+  watch('./src/img/**/**', () => gulp.start('images-dev'))
+})
 
-  gulp.watch('./**/**.scss', ['styles']);
-  gulp.watch('./js/*.js', ['scripts']);
-  gulp.watch('./test/**.html').on('change', server.reload);
-  gulp.watch('./js/*.js').on('change', server.reload);
-});
+// Compila versión CSS para producción
+gulp.task('css', ['css-build', 'css-build-min'])
+gulp.task('build', ['styles-build', 'pug-build', 'scripts-build', 'images-build', 'cache', 'sitemap'])
+
+
+/*
+gulp.task('pug', () =>
+  gulp.src('./dev/pug/pages/*.pug')
+    .pipe(plumber())
+    .pipe(data(function(file) {
+      return JSON.parse(fs.readFileSync('./dev/data/casos-de-exito.json'))
+    }))
+    .pipe(pug())
+    .pipe(gulp.dest('./public'))
+)
+*/
