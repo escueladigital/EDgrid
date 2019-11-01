2781ed1b995ac9be8db21432fcd60705b73d1137
Alvaro Felipe <alvaro@ed.team>
2019-11-01 13:04:36 -0500

    Actualiza mensaje de bienvenida

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default () => (
       <title>EDBoilerplate</title>
     </Helmet>
     <header className="s-bg-grey s-center s-pxy-4">
-      <h1>EDgrid</h1>
+      <h1>EDgrid 3.0 is coming</h1>
       <p className="s-mb-0 s-opacity-7">Creado con 💓 y 🦄 por <b>EDteam</b></p>
     </header>
   </>
